1e4d0c8e92c22153c1dd8e9f1550b3d2e96e3758
tan1ty <tatiana.kovalenko1992@gmail.com>
2023-08-23 19:05:48 +0300

    feat: add Add task feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { fetchTodos, addTodo } from './api';
-import { ITodo } from './interface';
+import { fetchTodos, addTodo, updateTodo, deleteTodo } from './api';
+import { NewTodo, TodoToUpdate, ITodo } from './interface';
 
 const App: React.FC = () => {
+  const [newTask, setNewTask] = useState({
+    name: '',
+    status: false,
+    description: '',
+  });
+
   const [todos, setTodos] = useState<ITodo[]>([]);
 
   useEffect(() => {
@@ -17,26 +23,38 @@ const App: React.FC = () => {
       .catch((err: Error) => console.log(err));
   };
 
-  const handleSaveTodo = (formData: ITodo): void => {
+  const handleSaveTodo = (formData: NewTodo): void => {
     addTodo(formData)
       .then(({ data }) => {
         setTodos(data.todos);
+        setNewTask({ name: '', status: false, description: '' });
       })
+
       .catch((err) => console.log(err));
   };
-
-  // your implementation for handleUpdateTodo
-  // your implementation for handleDeleteTodo
 
   return (
     <main className="App">
       <h1>To-do list</h1>
-      {todos.map((todo: ITodo) => (
+      <input
+        className="new-task"
+        type="text"
+        value={newTask.name}
+        placeholder="Name you task"
+        onChange={(e) => setNewTask({ ...newTask, name: e.target.value })}
+      />
+      <input
+        placeholder="Description"
+        value={newTask.description}
+        onChange={(e) =>
+          setNewTask({ ...newTask, description: e.target.value })
+        }
+      />
+      <button onClick={() => handleSaveTodo(newTask)}>AddTask</button>
+      {todos.map((todo) => (
         <li key={todo._id}>
-          <span>
-            {todo.name}: Completed: &nbsp; {todo.status.toString()}
-          </span>
-          <div>Description: &nbsp; {todo.description}</div>
+          <span>{todo.name}</span>
+          <input type="text" value={todo.description} />
         </li>
       ))}
     </main>
